7d132068d3a6240c3d022c22cd182d70da2bcb93
Nika Varsimashvili <nikusha1401@gmail.com>
2025-06-08 12:39:58 +0400

    remove manualChunk func

--- vite.config.js
+++ vite.config.js
@@ -6,20 +6,20 @@ import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), compression(), visualizer({ open: false })],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("react") || id.includes("react-dom"))
-              return "react-vendor"; // ან უბრალოდ 'react'
-            if (id.includes("gsap")) return "gsap";
-            if (id.includes("three")) return "three";
-            if (id.includes("swiper")) return "swiper";
-            return "vendor";
-          }
-        },
-      },
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks(id) {
+  //         if (id.includes("node_modules")) {
+  //           if (id.includes("react") || id.includes("react-dom"))
+  //             return "react-vendor"; 
+  //           if (id.includes("gsap")) return "gsap";
+  //           if (id.includes("three")) return "three";
+  //           if (id.includes("swiper")) return "swiper";
+  //           return "vendor";
+  //         }
+  //       },
+  //     },
+  //   },
+  // },
 });
