087189a7676a348d071e771f65199fe962a333b1
Nika Varsimashvili <nikusha1401@gmail.com>
2025-06-08 12:38:01 +0400

    resolve manulChank func

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import compression from 'vite-plugin-compression'
-import { visualizer } from 'rollup-plugin-visualizer'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import compression from "vite-plugin-compression";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    compression(), 
-    visualizer({ open: false }) 
-  ],
+  plugins: [react(), tailwindcss(), compression(), visualizer({ open: false })],
   build: {
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) return 'react'
-            if (id.includes('gsap')) return 'gsap'
-            if (id.includes('three')) return 'three'
-            if (id.includes('swiper')) return 'swiper'
-            return 'vendor'
+          if (id.includes("node_modules")) {
+            if (id.includes("react") || id.includes("react-dom"))
+              return "react-vendor"; // ან უბრალოდ 'react'
+            if (id.includes("gsap")) return "gsap";
+            if (id.includes("three")) return "three";
+            if (id.includes("swiper")) return "swiper";
+            return "vendor";
           }
-        }
-      }
-    }
-  }
-})
+        },
+      },
+    },
+  },
+});
